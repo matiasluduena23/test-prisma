979239b36272440d296a271d743b9ed8f8ab160d
Matias Luduena <matiasluduena23@gmail.com>
2025-04-18 17:06:25 -0300

    test

--- lib/database.ts
+++ lib/database.ts
@@ -3,13 +3,15 @@ import prisma from "./prisma";
 export async function getUsers() {
 	try {
 		const users = await prisma.user.findMany();
-
+		console.log(users);
 		return users;
 	} catch (error) {
 		if (error instanceof Error) {
 			console.error("Error getting the users", error.message);
+			throw new Error("Error en la base de datos");
 		} else {
 			console.log("Error en get users", error);
+			throw new Error("Error en la base de datos");
 		}
 	}
 }
